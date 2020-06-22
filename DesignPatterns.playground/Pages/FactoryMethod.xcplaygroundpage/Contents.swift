2f4de4b40b9c57926e6239c34bd239351490bb2c
pamnovalli <pamela.ianovalli@ifood.com.br>
2020-06-21 21:35:10 -0300

    Added Factory Method example

--- DesignPatterns.playground/Pages/FactoryMethod.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/FactoryMethod.xcplaygroundpage/Contents.swift
@@ -1,1 +1,78 @@
+// Factory method
 
+protocol Phone {
+    var camera: String { get }
+    
+    func call()
+    func takePictures()
+}
+
+final class iPhone: Phone {
+    var camera: String = "camera"
+    
+    func call() {
+        print("I'm calling fom iPhone.")
+    }
+    
+    func takePictures() {
+        print("I'm taking pictures with the iPhone \(camera).")
+    }
+}
+
+final class Samsung: Phone {
+    var camera: String = "camera"
+    
+    func call() {
+        print("I'm calling from Samsung.")
+    }
+    
+    func takePictures() {
+        print("I'm taking pictures with the Samsung \(camera).")
+    }
+}
+
+protocol PhoneFactory {
+    func make() -> Phone
+}
+
+final class iPhoneFactory: PhoneFactory {
+    func make() -> Phone {
+        return iPhone()
+    }
+    
+}
+
+final class SamsungFactory: PhoneFactory {
+    func make() -> Phone {
+        return Samsung()
+    }
+}
+
+let iphoneFactory = iPhoneFactory()
+let iphone = iphoneFactory.make()
+iphone.call()
+
+
+// And we can add a new type without changing the original code base.
+
+final class Xiaomi: Phone {
+    var camera: String = "camera"
+    
+    func call() {
+        print("I'm calling from Xiaomi.")
+    }
+    
+    func takePictures() {
+        print("I'm taking pictures with the Xiaomi \(camera).")
+    }
+}
+
+final class XiaomiFactory: PhoneFactory {
+    func make() -> Phone {
+        return Xiaomi()
+    }
+}
+
+let xiaomiFactory = XiaomiFactory()
+let xiaomi = xiaomiFactory.make()
+xiaomi.takePictures()
