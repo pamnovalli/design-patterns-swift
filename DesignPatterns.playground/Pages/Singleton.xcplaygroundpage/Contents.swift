fea706e930fdc13e572ecb5a6aa11eac95fdd1c0
pamnovalli <pamela.ianovalli@ifood.com.br>
2020-06-21 22:34:04 -0300

    Added Singleton example

--- DesignPatterns.playground/Pages/Singleton.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Singleton.xcplaygroundpage/Contents.swift
@@ -1,1 +1,14 @@
+// Singleton
 
+final class NotificationMannager {
+    static let sharedInstance = NotificationMannager()
+    
+    private init() {}
+    
+    func send() {
+        print("Sending notification")
+    }
+}
+
+let formatter = NotificationMannager.sharedInstance
+formatter.send()
