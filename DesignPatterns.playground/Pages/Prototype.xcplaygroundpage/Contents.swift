c339185fed380e54cf4fd59189992d6d92db3915
pamnovalli <pamela.ianovalli@ifood.com.br>
2020-06-21 21:54:48 -0300

    Added Prototype example

--- DesignPatterns.playground/Pages/Prototype.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Prototype.xcplaygroundpage/Contents.swift
@@ -1,1 +1,15 @@
+// Prototype
+
+struct Developer {
+    let languages: [String]
+    
+    func clone() -> Self {
+        return .init(languages: languages)
+    }
+}
+
+let developer = Developer(languages: ["swift", "objective-c", "kotlin"])
+
+let prototype = developer.clone()
+prototype.languages
 
