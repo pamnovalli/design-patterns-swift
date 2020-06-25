5467b70e6d068cbfe1a5432ed5622fe92741818e
Pamela Ianovalli <pamela.ianovalli@ifood.com.br>
2020-06-24 23:51:28 -0300

    Added Bridge pattern

--- DesignPatterns.playground/Pages/Bridge.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Bridge.xcplaygroundpage/Contents.swift
@@ -3,4 +3,46 @@
 */
 // Bridge
 
+protocol Phone {
+    func call()
+}
 
+protocol ColoredPhone {
+    var phone: Phone { get set}
+    
+    func call()
+}
+
+final class iPhone: Phone {
+    func call() {
+        print("Calling...")
+    }
+}
+
+final class Samsung: ColoredPhone {
+    var phone: Phone
+    
+    init(phone: Phone) {
+        self.phone = phone
+    }
+    
+    func call() {
+        phone.call()
+    }
+}
+
+final class PinkIPhone: ColoredPhone {
+    var phone: Phone
+    
+    init(phone: Phone) {
+        self.phone = phone
+    }
+    
+    func call() {
+        phone.call()
+    }
+}
+
+let iphone = iPhone()
+let pinkIphone = PinkIPhone(phone: iphone)
+pinkIphone.call()
