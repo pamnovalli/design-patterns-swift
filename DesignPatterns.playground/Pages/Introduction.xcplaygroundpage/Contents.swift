0b8c064515c033385bfc1864512b8f753b1a0bb8
Pamela Ianovalli <pamela.ianovalli@ifood.com.br>
2020-06-25 00:26:42 -0300

    Updated introduction

--- DesignPatterns.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -1,4 +1,34 @@
 /*:
- [< Previous](@previous)           [Home](Introduction)           [Next >](@next)
+# Design Patterns in Swift
+* Created by [Pamela Ianovalli](https://www.linkedin.com/in/pam-novalli/)
  
+This playground presents examples of implementation of the 23 Design Patterns in Swift.
+&nbsp;
+## Contents
+* [Abstract Factory](AbstractFactory)
+* [Builder](Builder)
+* [Factory Method](FactoryMethod)
+* [Prototype](Prototype)
+* [Singleton](Singleton)
+* [Adapter](Adapter)
+* [Bridge](Bridge)
+* [Composite](Composite)
+* [Decorator](Decorator)
+* [Facade](Facade)
+* [Flyweight](Flyweight)
+* [Proxy](Proxy)
+* [Chain Of Responsibility](ChainOfResponsibility)
+* [Command](Command)
+* [Interpreter](Interpreter)
+* [Mediator](Mediator)
+* [Memento](Memento)
+* [Observer](Observer)
+* [State](State)
+* [Strategy](Strategy)
+* [Template Method](TemplateMethod)
+* [Visitor](Visitor)
+* [Interator](Interator)
+&nbsp;
+
+[Next >](@next)
 */
