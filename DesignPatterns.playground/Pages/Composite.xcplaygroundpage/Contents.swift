09198d97f532b5e4206db73c08e7eafa0468c356
Pamela Ianovalli <pamela.ianovalli@ifood.com.br>
2020-06-26 22:59:25 -0300

    Added Composite pattern

--- DesignPatterns.playground/Pages/Composite.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Composite.xcplaygroundpage/Contents.swift
@@ -1,3 +1,40 @@
 /*:
  [< Previous](@previous)           [Home](Introduction)           [Next >](@next)
 */
+// Composite
+
+protocol Food {
+    func eat()
+}
+
+final class Hamburger: Food {
+    func eat() {
+        print("I'm eating Hamburguer")
+    }
+}
+
+final class FrenchFries: Food {
+    func eat() {
+        print("I'm eating French Fries")
+    }
+}
+
+final class Lunch: Food {
+    private var foods = [Food]()
+    
+    init(foods: [Food]) {
+        self.foods = foods
+    }
+    
+    func eat() {
+        for food in foods {
+            food.eat()
+        }
+    }
+}
+
+let fries = FrenchFries()
+let hamburger = Hamburger()
+
+let lunch = Lunch(foods: [fries, hamburger])
+lunch.eat()
