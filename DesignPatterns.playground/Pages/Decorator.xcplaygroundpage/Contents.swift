f8907d0146abd2a6a64b97f06dd0188a6d976cfd
Pamela Ianovalli <pamela.ianovalli@ifood.com.br>
2020-06-28 18:50:57 -0300

    Added decorator pattern

--- DesignPatterns.playground/Pages/Decorator.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Decorator.xcplaygroundpage/Contents.swift
@@ -1,3 +1,48 @@
 /*:
  [< Previous](@previous)           [Home](Introduction)           [Next >](@next)
 */
+// Decorator
+
+protocol Phone {
+    func call()
+    func unlock()
+}
+
+final class IPhone: Phone {
+    func unlock() {
+        print("Unlock")
+    }
+    
+    func call() {
+        print("Calling ...")
+    }
+}
+
+protocol IPhoneDecorator: Phone {
+    var phone: Phone { get set }
+}
+
+final class IPhone11: IPhoneDecorator {
+    var phone: Phone
+    
+    init(phone: Phone) {
+        self.phone = phone
+    }
+    
+    func unlock() {
+         unlockwithFaceId()
+    }
+    
+    func call() {
+        phone.call()
+    }
+    
+    func unlockwithFaceId() {
+        print("Unlock with face ID")
+    }
+}
+
+let iphone = IPhone()
+let iphone11 = IPhone11(phone: iphone)
+iphone11.call()
+iphone11.unlockwithFaceId()
