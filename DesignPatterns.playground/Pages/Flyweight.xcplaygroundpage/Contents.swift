a3cfcc5664bff00c1900594cc2167f4c53524574
Pamela Ianovalli <pamela.ianovalli@ifood.com.br>
2020-07-01 22:16:44 -0300

    Added flyweight pattern

--- DesignPatterns.playground/Pages/Flyweight.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Flyweight.xcplaygroundpage/Contents.swift
@@ -1,3 +1,28 @@
 /*:
  [< Previous](@previous)           [Home](Introduction)           [Next >](@next)
 */
+// Flyweight
+
+struct Habit {
+    var identifier: String
+    var daysPerMonth: Int
+}
+
+final class HabitTracker {
+    var habits: [String: Habit] = [String: Habit]()
+    
+    func addHabit(identifier: String, daysPerMonth: Int) -> Habit {
+        if let habbit = habits[identifier] {
+            return habbit
+        } else {
+            let newHabbit = Habit(identifier: identifier, daysPerMonth: daysPerMonth)
+            habits[identifier] = newHabbit
+            return newHabbit
+        }
+    }
+}
+
+
+let habitTracker = HabitTracker()
+
+habitTracker.addHabit(identifier: "read", daysPerMonth: 30)
